dd25df4e4c700ef9413b97ee84500dfdb138b0c2
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2024-02-08 22:15:54 +0530

    Add files via upload

---
+++ feb/linkedlist/ListCycle.java
@@ -0,0 +1,15 @@
+package leetcode.feb.linkedlist;
+
+public class ListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow_pointer = head, fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
