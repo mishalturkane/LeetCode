47abfaa0cb88f1e5a69ddb17e7088d68b9e5d42d
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2024-02-18 22:02:34 +0530

    Add files via upload

---
+++ feb/arrays/FirstMissingHashset.java
@@ -0,0 +1,23 @@
+package leetcode.feb.arrays;
+
+import java.util.HashSet;
+
+public class FirstMissingHashset {
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            HashSet<Integer> set=new HashSet<>();
+            int n=nums.length;
+            for(int num:nums){
+                if(num>0){
+                    set.add(num);
+                }
+            }
+            for(int i=1;i<=n+1;i++){
+                if(!set.contains(i)){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
