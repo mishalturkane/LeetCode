3a99a91f290c937a832031cb5f0b05b431d76c76
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2024-03-28 21:16:53 +0530

    Add files via upload

---
+++ march/MaxSubarrayLength.java
@@ -0,0 +1,22 @@
+package leetcode.march;
+
+import java.util.HashMap;
+
+public class MaxSubarrayLength {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int ans = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            mp.put(nums[r], mp.getOrDefault(nums[r], 0) + 1);
+            while (mp.get(nums[r]) > k) {
+                mp.put(nums[l], mp.get(nums[l]) - 1);
+                l++;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
